35a1232bb785ed68e27cd3a2d569c2945e13a1c0
emanuelrs01 <105462437+emanuelrs01@users.noreply.github.com>
2022-05-12 17:19:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+#llamar librerias
+from dash import Dash, html,dcc #Libreria para servidor web de Data science
 
+#Declarar Objetos Principales
+app = Dash(__name__)
+
+#Configurar el sitio web
+app.layout = html.Div([html.H1('Trabajo Emanuel Rios Silva'),
+                       html.Div('Emanuel Rios Silva Parte 2')])
+
+#Funcion Principal
+if __name__ == '__main__':
+  #Asi se carga el objeto principal a la interfaces de red con el puerto 80
+  app.run_server(host='0.0.0.0',port=80)
+  
+  
